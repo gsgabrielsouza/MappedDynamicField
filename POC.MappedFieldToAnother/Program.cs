32d69c06f7bfa8b1ff5715ebbca2716e98242ac1
gsgabrielsouza <souzagabrielluis@gmail.com>
2020-03-31 12:22:35 -0300

    tests with auto mapper

--- POC.MappedFieldToAnother/Program.cs
+++ POC.MappedFieldToAnother/Program.cs
@@ -39,60 +39,61 @@ namespace POC.MappedFieldToAnother
 
         static void AutoMapper<TSource, TDestination>(IEnumerable<IPropertyMap> propertiesMap, TSource source, TDestination destination)
         {
-            Action<IMapperConfigurationExpression> expression = x =>
-            {
-                var map = x.CreateMap<TSource, TDestination>();
-
-                foreach (var propertyMap in propertiesMap)
-                {
-                    var peSource = Expression.Parameter(source.GetType(), "src");
-                    var sourceExpression = Expression.Property(peSource, propertyMap.Source);
-                    // src => src.Property
-                    var sourceMapFromExpression =
-                        Expression.Lambda<Func<TSource, object>>(
-                            Expression.Convert(sourceExpression, typeof(object)), new ParameterExpression[] { peSource });
-
-                    var peDestination = Expression.Parameter(destination.GetType(), "dest");
-                    if (propertyMap.Destination.IndexOf('.') >= 0)
-                    {
-                        Expression destinationExpression = peDestination;
-                        foreach (var navigatinoPropertieDestination in propertyMap.Destination.Split('.'))
-                        {
-                            destinationExpression = Expression.Property(destinationExpression, navigatinoPropertieDestination);
-
-                            //// dest => dest.Property
-                            var destinationMapFromExpression =
-                                Expression.Lambda<Func<TDestination, object>>
-                                    (Expression.Convert(destinationExpression, typeof(object)), new ParameterExpression[] { peDestination });
-                            map.ForMember(destinationMapFromExpression, a => a.MapFrom(sourceMapFromExpression));
-                        }
-                    }
-                    else
-                    {
-                        var destinationExpression = Expression.Property(peDestination, propertyMap.Destination);
-
-                        // dest => src.Property
-                        var destinationMapFromExpression = Expression.Lambda<Func<TDestination, object>>(
-                            Expression.Convert(destinationExpression, typeof(object)), new ParameterExpression[] { peDestination });
-
-                        map.ForMember(destinationMapFromExpression, a => a.MapFrom(sourceMapFromExpression));
-                    }
-                }
-            };
-
-            configuration = new MapperConfiguration(expression);
-            //configuration = new MapperConfiguration(x =>
+            //Action<IMapperConfigurationExpression> expression = x =>
             //{
-            //    //x.CreateMap<EcommerceOrderDTO, Order>()
-            //    //    .ForMember(x => x.Shipping, a => a.Ignore());
-            //    //    x.CreateMap<Shipping, EcommerceOrderDTO>();
-            //    //x.CreateMap<EcommerceOrderDTO, Shipping>()
-            //    //    .ForMember("Method", a => a.MapFrom("Shipping"));
-            //    //x.CreateMap<EcommerceOrderDTO, Order>()
-            //    //    .ForMember(x => x.Shipping.Method, a => a.MapFrom(e => e.Shipping));
-            //    //x.CreateMap<EcommerceOrderDTO, Order>()
-            //    //    .ForMember("Shipping", a => a.MapFrom(""))
-            //});
+            //    var map = x.CreateMap<TSource, TDestination>();
+
+            //    foreach (var propertyMap in propertiesMap)
+            //    {
+            //        var peSource = Expression.Parameter(source.GetType(), "src");
+            //        var sourceExpression = Expression.Property(peSource, propertyMap.Source);
+            //        // src => src.Property
+            //        var sourceMapFromExpression =
+            //            Expression.Lambda<Func<TSource, object>>(
+            //                Expression.Convert(sourceExpression, typeof(object)), new ParameterExpression[] { peSource });
+
+            //        var peDestination = Expression.Parameter(destination.GetType(), "dest");
+            //        if (propertyMap.Destination.IndexOf('.') >= 0)
+            //        {
+            //            Expression destinationExpression = peDestination;
+            //            foreach (var navigatinoPropertieDestination in propertyMap.Destination.Split('.'))
+            //            {
+            //                destinationExpression = Expression.Property(destinationExpression, navigatinoPropertieDestination);
+
+            //                //// dest => dest.Property
+            //                var destinationMapFromExpression =
+            //                    Expression.Lambda<Func<TDestination, object>>
+            //                        (Expression.Convert(destinationExpression, typeof(object)), new ParameterExpression[] { peDestination });
+            //                map.ForMember(destinationMapFromExpression, a => a.MapFrom(sourceMapFromExpression));
+            //            }
+            //        }
+            //        else
+            //        {
+            //            var destinationExpression = Expression.Property(peDestination, propertyMap.Destination);
+
+            //            // dest => src.Property
+            //            var destinationMapFromExpression = Expression.Lambda<Func<TDestination, object>>(
+            //                Expression.Convert(destinationExpression, typeof(object)), new ParameterExpression[] { peDestination });
+
+            //            map.ForMember(destinationMapFromExpression, a => a.MapFrom(sourceMapFromExpression));
+            //        }
+            //    }
+            //};
+
+            //configuration = new MapperConfiguration(expression);
+
+            configuration = new MapperConfiguration(x =>
+            {
+                x.CreateMap<EcommerceOrderDTO, Order>()
+                    .ForMember(x => x.Shipping, a => a.Ignore());
+                //    x.CreateMap<Shipping, EcommerceOrderDTO>();
+                //x.CreateMap<EcommerceOrderDTO, Shipping>()
+                //    .ForMember("Method", a => a.MapFrom("Shipping"));
+                //x.CreateMap<EcommerceOrderDTO, Order>()
+                //    .ForMember(x => x.Shipping.Method, a => a.MapFrom(e => e.Shipping));
+                //x.CreateMap<EcommerceOrderDTO, Order>()
+                //    .ForMember("Shipping", a => a.MapFrom(""))
+            });
             var mapper = configuration.CreateMapper();
 
             var result = mapper.Map<Order>(source);
